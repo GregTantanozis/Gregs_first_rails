fc8b77867a7fcab8310d706d7b56a49ffc84480b
Grigorios Tantanozis <tarzanbros@gmail.com>
2018-02-15 17:48:49 +0100

    added authorization, changed routes and product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ApplicationRecord
+	validates :name, presence: true
 	has_many :orders
 	def self.search(search_term)
-  		Product.where("name ilike ?", "%#{search_term}%")
-	end
+	    if Rails.env.production?
+	      Product.where("name ilike ?", "%#{search_term}%")
+	    else
+	      Product.where("name LIKE ?", "%#{search_term}%")
+	    end
+  	end
 end
-
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
 
   get 'simple_pages/thank_you'
   post 'simple_pages/thank_you'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
