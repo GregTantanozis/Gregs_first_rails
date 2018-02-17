162ef601263df79c2351a1c3faa5d1a98a048b67
Grigorios Tantanozis <tarzanbros@gmail.com>
2018-02-18 00:58:06 +0100

    styled products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { registrations: "registrations" }
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
 
@@ -13,5 +16,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/thank_you'
   post 'simple_pages/thank_you'
+
+  get 'devise/users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
