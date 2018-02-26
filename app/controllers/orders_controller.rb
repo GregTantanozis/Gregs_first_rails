cbfcb28917bb94b71c58c592d6892639f97fb46e
Grigorios Tantanozis <tarzanbros@gmail.com>
2018-02-26 21:49:44 +0100

    removed pagination from orders controller and views/index cause there is a syntax error in the controller pagination code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @orders = Order.includes(:product).all.paginate(:page => params[:page], :per_page => 6)
+    @orders = Order.includes(:product).all
   end
 
   def show
